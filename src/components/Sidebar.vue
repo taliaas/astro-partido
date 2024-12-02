a054e2ff37ef604d1d653f98f63ef7d32ee2c04b
taliaas <taliacarruesalas@gmail.com>
2024-12-02 17:16:14 -0500

    sidebar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h2>Menu</h2>
+    </div>
+</template>
+
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,3 +1,5 @@
-<div>
-    hola,lola
-</div>
+<template>
+    <div>
+        <h1 class="text-3xl font-bold mb-6">Dashboard</h1>
+    </div>
+</template>
