2af0379484d8747c730330950cbb4bbe9efba76f
taliaas <taliacarruesalas@gmail.com>
2025-03-06 00:09:47 -0500

    feat: implement attendance method

--- src/services/OrdinaryService.ts
+++ src/services/OrdinaryService.ts
@@ -71,6 +71,7 @@ export default class OrdinaryService {
   }
 
   async updateMinute(id: string, acta: any) {
+    console.log(acta)
     try {
       const response = await fetch(`${API_URL}/${id}`, {
         method: "PATCH",
@@ -190,6 +191,24 @@ export default class OrdinaryService {
     }
     catch (e) {
       console.error(e)
+    }
+  }
+
+  async getAttendance(month: number, year: number){
+    try {
+      const response = await fetch(`${API_URL}/attendance/${month}/${year}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return await response.json();
+    } catch (error) {
+      console.error(error);
+      throw error;
     }
   }
 }
