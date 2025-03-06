4256b1b3db69bfd9f5560f9c293a649e7a278cd4
taliaas <taliacarruesalas@gmail.com>
2025-03-06 00:14:02 -0500

    feat: create indicator

---
+++ src/components/Indicators/CreateIndicator.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="max-w-[1600px] mx-auto space-y-6 p-4">
+    <div class="p-6 rounded border border-gray-200 bg-white shadow-sm">
+      <div class="mb-8 flex justify-between items-center">
+        <h1 class="text-2xl font-bold text-gray-900 pb-2">
+          Gestión de Indicadores
+        </h1>
+      </div>
+      <div>
+        <div>
+          <label>Nombre</label>
+          <input class="flex"></input>
+        </div>
+        <div>
+          <label>Descripción</label>
+          <textarea class="w-full"></textarea>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import {Label} from "@/components/ui/label";
+import {Input} from "@/components/ui/input";
+
+
+</script>
