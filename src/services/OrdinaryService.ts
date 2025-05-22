b12419480e52a8384abd7874057eec716afdd217
taliaas <taliacarruesalas@gmail.com>
2025-05-22 13:33:29 -0400

    feat: delete lastest

--- src/services/OrdinaryService.ts
+++ src/services/OrdinaryService.ts
@@ -37,24 +37,6 @@ export default class OrdinaryService {
     }
   }
 
-  //obtener las actas recientes
-  async getLatestMinute() {
-    try {
-      const response = await fetch(`${API_URL}/minutes-ordinary/latest`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      return await response.json();
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
   async getMinutesByCurrentMonth() {
     try {
       const response = await fetch(`${API_URL}/minutes-ordinary/byMonth`, {
