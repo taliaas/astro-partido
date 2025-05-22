6100accf20a2111077ddcee8d9fb395190ae6487
Altro <albe020531@outlook.com>
2025-05-22 12:22:24 -0400

    refactor: clean up formatting and structure in minute action file for improved readability

--- src/actions/minute.ts
+++ src/actions/minute.ts
@@ -1,57 +1,50 @@
-import {ActionError, defineAction} from "astro:actions"
-import {z} from "astro:schema";
-import {getSession} from "auth-astro/server.ts";
-import {API_URL} from "astro:env/client";
+import { ActionError, defineAction } from "astro:actions";
+import { z } from "astro:schema";
+import { getSession } from "auth-astro/server.ts";
+import { API_URL } from "astro:env/client";
 
 export const uploadMinutes = defineAction({
-    accept: "form",
-    input: z.object({
-        files: z.any(),
-        type: z.string().refine(val => ["ro", "cp"].includes(val))
-    }),
-    async handler({files, type}, context) {
-        const session = await getSession(context.request);
-        if (!session) throw new ActionError({code: "UNAUTHORIZED"});
+  accept: "form",
+  input: z.object({
+    files: z.any(),
+    type: z.string().refine((val) => ["ro", "cp"].includes(val)),
+  }),
+  async handler({ files, type }, context) {
+    const session = await getSession(context.request);
+    if (!session) throw new ActionError({ code: "UNAUTHORIZED" });
 
-        const res = await fetch(
-            `${API_URL}/minutes/upload?type=${type}`,
-            {
-                method: "POST",
-                body: files,
-                headers: {
-                    Authorization: `${session.jwt}`
-                }
-            },
-        );
-        if (!res.ok) {
-            throw new Error(`HTTP error! status: ${res.status}`);
-        }
-        return res.json();
-    },
-})
+    const res = await fetch(`${API_URL}/minutes/upload?type=${type}`, {
+      method: "POST",
+      body: files,
+      headers: {
+        Authorization: `${session.jwt}`,
+      },
+    });
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+    return res.json();
+  },
+});
 
 export const deleteMinute = defineAction({
-    input: z.object({
-        id: z.number(),
-        type: z.string().refine(val => ["ro", "cp"].includes(val))
-    }),
-    async handler({id, type}, context){
-        const session = await getSession(context.request);
-        if (!session) throw new ActionError({code: "UNAUTHORIZED"});
+  input: z.object({
+    id: z.number(),
+    type: z.string().refine((val) => ["ro", "cp"].includes(val)),
+  }),
+  async handler({ id, type }, context) {
+    const session = await getSession(context.request);
+    if (!session) throw new ActionError({ code: "UNAUTHORIZED" });
 
-        const res = await fetch(
-            `${API_URL}/minutes/delete/${id}?type=${type}`,
-            {
-                method: "DELETE",
-                body: files,
-                headers: {
-                    Authorization: `${session.jwt}`
-                }
-            },
-        );
-        if (!res.ok) {
-            throw new Error(`HTTP error! status: ${res.status}`);
-        }
-        return res.json();
+    const res = await fetch(`${API_URL}/minutes/delete/${id}?type=${type}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `${session.jwt}`,
+      },
+    });
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
     }
-})
+    return res.json();
+  },
+});
